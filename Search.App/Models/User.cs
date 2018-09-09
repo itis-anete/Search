af28ed10553b420137a91b38a5f1d0bf1215a7e6
unknown <daryasa1991@mail.ru>
2018-09-09 17:04:29 +0300

    Создала класс User-его поля, добавила надпись на начальную страницу

---
+++ Search.App/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Search.App.Models
+{
+    public class User
+    {
+        public string Nickname { get; set;}
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public enum Sex { male, female, undefined }
+        public DateTime Birthday { get; set; }
+        // Country: ? (можно создать таблицу Countries)
+       // public string Countries { get; set; }
+    }
+}
