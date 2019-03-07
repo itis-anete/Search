d4604c1b0ae7d5725102b466b538673f029507b5
DrunkRussianGun <maxim71515@gmail.com>
2019-03-07 22:40:31 +0300

    Socket client and server are more concrete now

--- Search.Web/Controllers/SocketsController.cs
+++ Search.Web/Controllers/SocketsController.cs
@@ -12,12 +12,12 @@ namespace Search.API.Controllers
         [HttpGet("{message}")]
         public IActionResult Get(string message)
         {
-            using (var server = SocketServer<string, string>.StartNew(
+            using (var server = SocketServer<string>.StartNew(
                 ip: "127.0.0.1",
                 port: 33060,
                 onRequest: request => new string(request.Reverse().ToArray())))
             {
-                var response = SocketClient<string, string>.Send("127.0.0.1", 33060, message);
+                var response = SocketClient<string>.Send("127.0.0.1", 33060, message);
                 return Ok(response);
             }
         }
