2ff6e3ae2a0562592cff8b529a8c31e71a07402d
DrunkRussianGun <maxim71515@gmail.com>
2019-03-07 18:03:28 +0300

    Add DocumentInfo entity

---
+++ Search.Database/Entities/DocumentInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Search.Database.Entities
+{
+    public class DocumentInfo
+    {
+        public string Url { get; set; }
+        public string Title { get; set; }
+
+        public Dictionary<string, int> CountOfWordsInText { get; set; }
+        public Dictionary<string, int> CountOfWordsInTags { get; set; }
+        public int TotalCountOfWords { get; set; }
+    }
+}
