aeb288bf47db1b7700b6676ae11eac0e8f6afd70
Daryayan <daryasa1991@mail.ru>
2019-03-11 19:33:41 +0300

    pro-pigeon

--- Search.Web/Parser.cs
+++ Search.Web/Parser.cs
@@ -10,23 +10,12 @@ namespace Search.API
 {
     public class Parser
     {
-        static void GetMetaInformation(HtmlDocument htmlDoc, string value)
-        {
-            HtmlNode tcNode = htmlDoc.DocumentNode.SelectSingleNode("//meta[@name='" + value + "']");
-            string description = string.Empty;
-            if (tcNode != null)
-            {
-                HtmlAttribute desc;
-                desc = tcNode.Attributes["content"];
-
-            }
-        }
-
         static Dictionary<int, string> SelectChildNodes(HtmlDocument html)
         {
             Dictionary<int, string> indexedInfo = new Dictionary<int, string>();
             var htmlBody = html.DocumentNode.SelectSingleNode("//body");
-            HtmlNodeCollection childNodes = htmlBody.ChildNodes;
+            IEnumerable<HtmlNode> childNodes;
+            childNodes = htmlBody.DescendantsAndSelf();
             foreach (var node in childNodes)
             {
                 if (node.NodeType == HtmlNodeType.Element)
@@ -36,71 +25,17 @@ namespace Search.API
             }
             return indexedInfo;
         }
-        //public void GetAllNodes(HtmlDocument html)
+
+        //static void GetMetaInformation(HtmlDocument htmlDoc, string value)
         //{
-        //    HtmlDocument htmlDoc = new HtmlDocument();
-        //    string strHtml = html.ToString();
-
-        //    HtmlNodeCollection nodes = htmlDoc.DocumentNode.ChildNodes;
-
-        //    foreach (HtmlNode node in nodes)
+        //    HtmlNode tcNode = htmlDoc.DocumentNode.SelectSingleNode("//meta[@name='" + value + "']");
+        //    string description = string.Empty;
+        //    if (tcNode != null)
         //    {
-        //        if (node.Name.ToLower() == "p")
-        //        {
-        //            Paragraph newPPara = new Paragraph();
-        //            foreach (HtmlNode childNode in node.ChildNodes)
-        //            {
-        //                InterNode(childNode, ref newPPara);
-        //            }
-        //            richTextBlock.Blocks.Add(newPPara);
-        //        }
+        //        HtmlAttribute desc;
+        //        desc = tcNode.Attributes["content"];
+
         //    }
         //}
-        //public bool InterNode(HtmlNode htmlNode, ref Paragraph originalPar)
-        //{
-        //    string htmlNodeName = htmlNode.Name.ToLower();
-
-        //    List<string> nodeAttList = new List<string>();
-        //    HtmlNode parentNode = htmlNode.ParentNode;
-        //    while (parentNode != null)
-        //    {
-        //        nodeAttList.Add(parentNode.Name);
-        //        parentNode = parentNode.ParentNode;
-        //    } //we need to get it multiple types, because it could be b(old) and i(talic) at the same time.
-
-        //    Inline newRun = new Run();
-        //    foreach (string noteAttStr in nodeAttList) //with this we can set all the attributes to the inline
-        //    {
-        //        switch (noteAttStr)
-        //        {
-        //            case ("b"):
-        //            case ("strong"):
-        //                {
-        //                    newRun.FontWeight = FontWeights.Bold;
-        //                    break;
-        //                }
-        //            case ("i"):
-        //            case ("em"):
-        //                {
-        //                    newRun.FontStyle = FontStyle.Italic;
-        //                    break;
-        //                }
-        //        }
-        //    }
-
-        //    if (htmlNodeName == "#text") //the #text means that its a text node. Like <i><#text/></i>. Thanks @HungCao
-        //    {
-        //        ((Run)newRun).Text = htmlNode.InnerText;
-        //    }
-        //    else //if it is not a #text, don't load its innertext, as it's another node and it will always have a #text node as a child (if it has any text)
-        //    {
-        //        foreach (HtmlNode childNode in htmlNode.ChildNodes)
-        //        {
-        //            InterNode(childNode, ref originalPar);
-        //        }
-        //    }
-        //    return true;
-        //}
-
     }
 }
