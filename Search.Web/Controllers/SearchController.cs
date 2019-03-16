8c7cd0828c03f9f3435c4be1cb8787413163265b
DrunkRussianGun <maxim71515@gmail.com>
2019-03-16 13:17:13 +0300

    Now url is unique for each document; documents can be removed from database

--- Search.Web/Controllers/SearchController.cs
+++ Search.Web/Controllers/SearchController.cs
@@ -14,21 +14,6 @@ namespace Search.Web.Controllers
             _searcher = searcher;
         }
 
-        // POST api/search
-        [HttpPost]
-        public ActionResult Index(
-            [FromQuery] string title,
-            [FromBody] string body)
-        {
-            var info = new DocumentInfo
-            {
-                Title = title,
-                Text = body
-            };
-            _searcher._searchDatabase.Add(info);
-            return Ok();
-        }
-
         // GET api/search
         [HttpGet]
         public ActionResult<IEnumerable<string>> Search([FromQuery] string query)
@@ -39,6 +24,30 @@ namespace Search.Web.Controllers
             };
 
             return Ok(_searcher.Search(searchQuery));
+        }
+
+        // POST api/search
+        [HttpPost]
+        public ActionResult Index(
+            [FromQuery] string title,
+            [FromBody] string body)
+        {
+            var info = new DocumentInfo
+            {
+                Url = title,
+                Title = title,
+                Text = body
+            };
+            _searcher._searchDatabase.Add(info);
+            return Ok();
+        }
+
+        // DELETE api/search
+        [HttpDelete]
+        public ActionResult Remove([FromQuery] string url)
+        {
+            _searcher._searchDatabase.Remove(url);
+            return Ok();
         }
 
         private readonly Searcher _searcher;
