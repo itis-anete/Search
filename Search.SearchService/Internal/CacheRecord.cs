2d9d2450db6a5a42a200580203e6d06ecf98de2e
DrunkRussianGun <maxim71515@gmail.com>
2019-04-06 12:53:04 +0300

    Now cache removes old records

---
+++ Search.SearchService/Internal/CacheRecord.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Search.SearchService.Internal
+{
+    public class CacheRecord
+    {
+        public SearchResponse Response { get; }
+        public TimeSpan CreationTime { get; set; }
+
+        public CacheRecord(SearchResponse response, TimeSpan creationTime)
+        {
+            Response = response;
+            CreationTime = creationTime;
+        }
+    }
+}
--- Search.SearchService/MemoryRequestCache.cs
+++ Search.SearchService/MemoryRequestCache.cs
@@ -2,33 +2,50 @@
 using Search.Core.Entities;
 using Search.Infrastructure;
 using Search.SearchService.Internal;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
 
 namespace Search.SearchService
 {
-    public class MemoryRequestCache : IRequestCache
+    public sealed class MemoryRequestCache : IRequestCache, IDisposable
     {
-        public MemoryRequestCache(ElasticSearchClient client, ElasticSearchOptions options)
+        public MemoryRequestCache(
+            ElasticSearchClient client,
+            ElasticSearchOptions options,
+            TimeSpan? maxRecordAge = null)
         {
             _options = options;
+            _maxRecordAge = maxRecordAge ?? TimeSpan.FromMinutes(5);
 
             client.OnIndex += ElasticSearchClient_OnIndex;
+
+            var intervalBetweenAgeChecks = _maxRecordAge / 2;
+            _ageStopwatch = Stopwatch.StartNew();
+            _ageCheckingTimer = new Timer(
+                TimerCallback,
+                null,
+                intervalBetweenAgeChecks,
+                intervalBetweenAgeChecks);
         }
 
         public void Add(SearchRequest request, SearchResponse response)
         {
-            _cache[request] = response;
+            _cache[request] = new CacheRecord(response, _ageStopwatch.Elapsed);
         }
 
         public SearchResponse GetResponse(SearchRequest request)
         {
-            if (_cache.TryGetValue(request, out var response))
-                return response;
+            if (_cache.TryGetValue(request, out var record))
+                return record.Response;
             return null;
         }
 
         public bool IsCached(SearchRequest request)
         {
+            CheckRecordAge(request, resetAge: true);
             return _cache.ContainsKey(request);
         }
 
@@ -39,12 +56,36 @@ namespace Search.SearchService
 
         public bool TryGetResponse(SearchRequest request, out SearchResponse response)
         {
-            return _cache.TryGetValue(request, out response);
+            response = null;
+            CheckRecordAge(request, resetAge: true);
+
+            if (!_cache.TryGetValue(request, out var record))
+                return false;
+
+            response = record.Response;
+            return true;
         }
 
+        public void Dispose()
+        {
+            if (_disposed)
+                return;
+
+            _ageCheckingTimer.Dispose();
+            _cache.Clear();
+            _ageStopwatch.Stop();
+
+            _disposed = true;
+        }
+
+        private readonly TimeSpan _maxRecordAge;
         private readonly ElasticSearchOptions _options;
-        private readonly Dictionary<SearchRequest, SearchResponse> _cache =
-                     new Dictionary<SearchRequest, SearchResponse>(Comparer);
+
+        private readonly Dictionary<SearchRequest, CacheRecord> _cache =
+                     new Dictionary<SearchRequest, CacheRecord>(Comparer);
+        private readonly Stopwatch _ageStopwatch;
+        private readonly Timer _ageCheckingTimer;
+        private bool _disposed;
 
         private static readonly RequestComparer Comparer = new RequestComparer();
 
@@ -57,6 +98,25 @@ namespace Search.SearchService
                 return;
 
             _cache.Clear();
+        }
+
+        private void TimerCallback(object state)
+        {
+            var keys = _cache.Keys.ToArray();
+            foreach (var request in keys)
+                CheckRecordAge(request);
+        }
+
+        private void CheckRecordAge(SearchRequest request, bool resetAge = false)
+        {
+            if (!_cache.TryGetValue(request, out var record))
+                return;
+
+            var currentAge = _ageStopwatch.Elapsed - record.CreationTime;
+            if (currentAge > _maxRecordAge)
+                _cache.Remove(request);
+            else if (resetAge)
+                record.CreationTime = _ageStopwatch.Elapsed;
         }
     }
 }
