dbaab5595bde4e83db7ab5e6c2f61d5332464e7d
Daryayan <daryasa1991@mail.ru>
2019-04-10 21:32:09 +0300

    IndexedPages model

---
+++ Search.Web/Indexing/HtmlIndexer.cs
@@ -0,0 +1,35 @@
+﻿using HtmlAgilityPack;
+using System.Text.Json;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Controls;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Text.RegularExpressions;
+using System.Reflection;
+using System.IO;
+
+
+namespace Search.Web.Indexing
+{
+    public class HtmlParser
+    {
+        static Dictionary<int, string> HtmlIndexer(HtmlDocument html)
+        {
+            Dictionary<int, string> indexedInfo = new Dictionary<int, string>();
+            var htmlBody = html.DocumentNode.SelectSingleNode("//body");
+            IEnumerable<HtmlNode> nodes;
+            nodes = htmlBody.DescendantsAndSelf();
+            foreach (var node in nodes)
+            {
+                if (node.NodeType == HtmlNodeType.Element)
+                {
+                    indexedInfo.Add(node.OuterHtml.GetHashCode(), node.OuterHtml);
+                }
+            }
+            return indexedInfo;
+        }
+    }
+}
