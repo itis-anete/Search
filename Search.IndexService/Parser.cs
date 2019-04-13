84061c3cdbb3bcf5fbaaee54165e47906efa101c
DrunkRussianGun <maxim71515@gmail.com>
2019-04-13 13:01:00 +0300

    Fix errors

--- Search.IndexService/Parser.cs
+++ Search.IndexService/Parser.cs
@@ -1,27 +1,20 @@
 ﻿using HtmlAgilityPack;
-using System.Text.Json;
-using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Windows.Controls;
-using System.Threading.Tasks;
-using System.Windows.Documents;
-using System.Text.RegularExpressions;
-using System.Reflection;
 using System.IO;
+using System.Reflection;
+using System.Text.RegularExpressions;
 
-namespace Search.API
+namespace Search.Web
 {
     class BirdFormatter
     {
-
         public static void UnBird(string text)
         {
-            List<Tuple<string, string, string>> UnBird = new List<Tuple<string, string, string>>();
-            Regex typeHeader = new Regex(@"neb{\w*neck");
-            Regex nameHeader = new Regex(@"neck\w* = ");
-            Regex valueHeader = new Regex(@" = \w*}tail,");
+            var UnBird = new List<Tuple<string, string, string>>();
+            var typeHeader = new Regex(@"neb{\w*neck");
+            var nameHeader = new Regex(@"neck\w* = ");
+            var valueHeader = new Regex(@" = \w*}tail,");
 
             MatchCollection typeMatches = typeHeader.Matches(text);
             MatchCollection nameMatches = nameHeader.Matches(text);
@@ -35,19 +28,21 @@ namespace Search.API
             }
 
         }
+
         public static void ToBird(List<Tuple<string, Type, string>> objInfo)
         {
-            string neb = "neb{";
-            string tail = "}tail,";
+            var neb = "neb{";
+            var tail = "}tail,";
             string bird;
             foreach (var prop in objInfo)
             {
                 bird = neb + prop.Item2 + "neck" + prop.Item1 + " = " + prop.Item3 + tail;
             }
         }
+
         public static async void ReadBird(string fileName)
         {
-            using (StreamReader reader = new StreamReader(fileName))
+            using (var reader = new StreamReader(fileName))
             {
                 string result = await reader.ReadToEndAsync();
             }
@@ -55,7 +50,7 @@ namespace Search.API
 
         public static async void WriteBird(string fileName, string text)
         {
-            using (StreamWriter writer = new StreamWriter(fileName, false))
+            using (var writer = new StreamWriter(fileName, false))
             {
                 await writer.WriteLineAsync(text);
             }
@@ -63,9 +58,9 @@ namespace Search.API
 
         public static List<Tuple<string, Type, string>> GetProperties(object obj)
         {
-            List<Tuple<string, Type, string>> objInfo = new List<Tuple<string, Type, string>>();
-            Type t = typeof(object);
-            PropertyInfo[] propInfos = t.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            var objInfo = new List<Tuple<string, Type, string>>();
+            var t = typeof(object);
+            var propInfos = t.GetProperties(BindingFlags.Public | BindingFlags.Instance);
             foreach (var prop in propInfos)
             {
                 objInfo.Add(
@@ -75,14 +70,14 @@ namespace Search.API
             return objInfo;
         }
     }
+
     public class Parser
     {
         static Dictionary<int, string> HtmlIndexer(HtmlDocument html)
         {
-            Dictionary<int, string> indexedInfo = new Dictionary<int, string>();
+            var indexedInfo = new Dictionary<int, string>();
             var htmlBody = html.DocumentNode.SelectSingleNode("//body");
-            IEnumerable<HtmlNode> nodes;
-            nodes = htmlBody.DescendantsAndSelf();
+            var nodes = htmlBody.DescendantsAndSelf();
             foreach (var node in nodes)
             {
                 if (node.NodeType == HtmlNodeType.Element)
@@ -92,19 +87,5 @@ namespace Search.API
             }
             return indexedInfo;
         }
-
-        //static Dictionary<int, string> JSONIndexer(JsonDocument json)
-        //{
-        //    Dictionary<int, string> indexedInfo = new Dictionary<int, string>();
-            
-
-        //    return indexedInfo;
-        //}
-
-        //static Dictionary<int, string> XMLIndexer()
-        //{
-        //    Dictionary<int, string> indexedInfo = new Dictionary<int, string>();
-        //    return indexedInfo;
-        //}
     }
 }
