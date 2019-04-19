b951c9044e5bf417804b0087c8a61a876386d8b2
Daryayan <daryasa1991@mail.ru>
2019-04-19 18:13:16 +0300

    parser

--- Search.Core/Entities/DocumentInfo.cs
+++ Search.Core/Entities/DocumentInfo.cs
@@ -8,5 +8,12 @@ namespace Search.Core.Entities
         public DateTime IndexedTime { get; set; }
         public string Title { get; set; }
         public string Text { get; set; }
+        public DocumentInfo(url url)
+        {
+            var doc = GetHtml(url);
+            Url = url;
+            Title = doc.DocumentNode.SelectSingleNode("//title");
+            Text = ConvertDoc(doc).Replace(Title, "");
+        }
     }
 }
--- Search.IndexService/Parser.cs
+++ Search.IndexService/Parser.cs
@@ -1,91 +1,209 @@
-﻿using HtmlAgilityPack;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.IO;
-using System.Reflection;
 using System.Text.RegularExpressions;
+using HtmlAgilityPack;
+using System.Threading.Tasks;
 
-namespace Search.Web
+namespace Search.IndexService
 {
-    class BirdFormatter
+    class Parser
     {
-        public static void UnBird(string text)
+        public static class HtmlToText
         {
-            var UnBird = new List<Tuple<string, string, string>>();
-            var typeHeader = new Regex(@"neb{\w*neck");
-            var nameHeader = new Regex(@"neck\w* = ");
-            var valueHeader = new Regex(@" = \w*}tail,");
-
-            MatchCollection typeMatches = typeHeader.Matches(text);
-            MatchCollection nameMatches = nameHeader.Matches(text);
-            MatchCollection valueMatches = valueHeader.Matches(text);
-            for (int i = 0; i <= typeMatches.Count; i++)
+            public static string Convert(string path)
             {
-                UnBird.Add(new Tuple<string, string, string>
-                     (nameMatches[i].Value.Replace("neb{", "").Replace("neck", ""),
-                     typeMatches[i].Value.Replace("neck", "").Replace(" = ", ""),
-                     valueMatches[i].Value.Replace(" = ", "").Replace("}tail,", "")));
+                HtmlDocument doc = new HtmlDocument();
+                doc.Load(path);
+                return ConvertDoc(doc);
             }
 
-        }
-
-        public static void ToBird(List<Tuple<string, Type, string>> objInfo)
-        {
-            var neb = "neb{";
-            var tail = "}tail,";
-            string bird;
-            foreach (var prop in objInfo)
+            public static string ConvertHtml(string html)
             {
-                bird = neb + prop.Item2 + "neck" + prop.Item1 + " = " + prop.Item3 + tail;
+                HtmlDocument doc = new HtmlDocument();
+                doc.LoadHtml(html);
+                return ConvertDoc(doc);
             }
-        }
 
-        public static async void ReadBird(string fileName)
-        {
-            using (var reader = new StreamReader(fileName))
+            public static string ConvertDoc(HtmlDocument doc)
             {
-                string result = await reader.ReadToEndAsync();
-            }
-        }
-
-        public static async void WriteBird(string fileName, string text)
-        {
-            using (var writer = new StreamWriter(fileName, false))
-            {
-                await writer.WriteLineAsync(text);
-            }
-        }
-
-        public static List<Tuple<string, Type, string>> GetProperties(object obj)
-        {
-            var objInfo = new List<Tuple<string, Type, string>>();
-            var t = typeof(object);
-            var propInfos = t.GetProperties(BindingFlags.Public | BindingFlags.Instance);
-            foreach (var prop in propInfos)
-            {
-                objInfo.Add(
-                    new Tuple<string, Type, string>
-                    (prop.Name, prop.PropertyType, (string)prop.GetValue(obj)));
-            }
-            return objInfo;
-        }
-    }
-
-    public class Parser
-    {
-        static Dictionary<int, string> HtmlIndexer(HtmlDocument html)
-        {
-            var indexedInfo = new Dictionary<int, string>();
-            var htmlBody = html.DocumentNode.SelectSingleNode("//body");
-            var nodes = htmlBody.DescendantsAndSelf();
-            foreach (var node in nodes)
-            {
-                if (node.NodeType == HtmlNodeType.Element)
+                using (StringWriter sw = new StringWriter())
                 {
-                    indexedInfo.Add(node.OuterHtml.GetHashCode(), node.OuterHtml);
+                    ConvertTo(doc.DocumentNode, sw);
+                    sw.Flush();
+                    return sw.ToString();
                 }
             }
-            return indexedInfo;
+
+            internal static void ConvertContentTo(HtmlNode node, TextWriter outText, PreceedingDomTextInfo textInfo)
+            {
+                foreach (HtmlNode subnode in node.ChildNodes)
+                {
+                    ConvertTo(subnode, outText, textInfo);
+                }
+            }
+            public static void ConvertTo(HtmlNode node, TextWriter outText)
+            {
+                ConvertTo(node, outText, new PreceedingDomTextInfo(false));
+            }
+            internal static void ConvertTo(HtmlNode node, TextWriter outText, PreceedingDomTextInfo textInfo)
+            {
+                string html;
+                switch (node.NodeType)
+                {
+                    case HtmlNodeType.Comment:
+                        break;
+                    case HtmlNodeType.Document:
+                        ConvertContentTo(node, outText, textInfo);
+                        break;
+                    case HtmlNodeType.Text:
+                        // script and style must not be output
+                        string parentName = node.ParentNode.Name;
+                        if ((parentName == "script") || (parentjuufName == "style"))
+                        {
+                            break;
+                        }
+                        // get text
+                        html = ((HtmlTextNode)node).Text;
+                        // is it in fact a special closing node output as text?
+                        if (HtmlNode.IsOverlappedClosingElement(html))
+                        {
+                            break;
+                        }
+                        // check the text is meaningful and not a bunch of whitespaces
+                        if (html.Length == 0)
+                        {
+                            break;
+                        }
+                        if (!textInfo.WritePrecedingWhiteSpace || textInfo.LastCharWasSpace)
+                        {
+                            html = html.TrimStart();
+                            if (html.Length == 0) { break; }
+                            textInfo.IsFirstTextOfDocWritten.Value = textInfo.WritePrecedingWhiteSpace = true;
+                        }
+                        outText.Write(HtmlEntity.DeEntitize(Regex.Replace(html.TrimEnd(), @"\s{2,}", " ")));
+                        if (textInfo.LastCharWasSpace = char.IsWhiteSpace(html[html.Length - 1]))
+                        {
+                            outText.Write(' ');
+                        }
+                        break;
+                    case HtmlNodeType.Element:
+                        string endElementString = null;
+                        bool isInline;
+                        bool skip = false;
+                        int listIndex = 0;
+                        switch (node.Name)
+                        {
+                            case "nav":
+                                skip = true;
+                                isInline = false;
+                                break;
+                            case "body":
+                            case "section":
+                            case "article":
+                            case "aside":
+                            case "h1":
+                            case "h2":
+                            case "header":
+                            case "footer":
+                            case "address":
+                            case "main":
+                            case "div":
+                            case "p": // stylistic - adjust as you tend to use
+                                if (textInfo.IsFirstTextOfDocWritten)
+                                {
+                                    outText.Write("\r\n");
+                                }
+                                endElementString = "\r\n";
+                                isInline = false;
+                                break;
+                            case "br":
+                                outText.Write("\r\n");
+                                skip = true;
+                                textInfo.WritePrecedingWhiteSpace = false;
+                                isInline = true;
+                                break;
+                            case "a":
+                                if (node.Attributes.Contains("href"))
+                                {
+                                    string href = node.Attributes["href"].Value.Trim();
+                                    if (node.InnerText.IndexOf(href, StringComparison.InvariantCultureIgnoreCase) == -1)
+                                    {
+                                        endElementString = "<" + href + ">";
+                                    }
+                                }
+                                isInline = true;
+                                break;
+                            case "li":
+                                if (textInfo.ListIndex > 0)
+                                {
+                                    outText.Write("\r\n{0}.\t", textInfo.ListIndex++);
+                                }
+                                else
+                                {
+                                    outText.Write("\r\n*\t");
+                                }
+                                isInline = false;
+                                break;
+                            case "ol":
+                                listIndex = 1;
+                                goto case "ul";
+                            case "ul": 
+                                endElementString = "\r\n";
+                                isInline = false;
+                                break;
+                            case "img": 
+                                if (node.Attributes.Contains("alt"))
+                                {
+                                    outText.Write('[' + node.Attributes["alt"].Value);
+                                    endElementString = "]";
+                                }
+                                if (node.Attributes.Contains("src"))
+                                {
+                                    outText.Write('<' + node.Attributes["src"].Value + '>');
+                                }
+                                isInline = true;
+                                break;
+                            default:
+                                isInline = true;
+                                break;
+                        }
+                        if (!skip && node.HasChildNodes)
+                        {
+                            ConvertContentTo(node, outText, isInline ? textInfo : new PreceedingDomTextInfo(textInfo.IsFirstTextOfDocWritten) { ListIndex = listIndex });
+                        }
+                        if (endElementString != null)
+                        {
+                            outText.Write(endElementString);
+                        }
+                        break;
+                }
+            }
+        }
+        internal class PreceedingDomTextInfo
+        {
+            public PreceedingDomTextInfo(BoolWrapper isFirstTextOfDocWritten)
+            {
+                IsFirstTextOfDocWritten = isFirstTextOfDocWritten;
+            }
+            public bool WritePrecedingWhiteSpace { get; set; }
+            public bool LastCharWasSpace { get; set; }
+            public readonly BoolWrapper IsFirstTextOfDocWritten;
+            public int ListIndex { get; set; }
+        }
+        internal class BoolWrapper
+        {
+            public BoolWrapper() { }
+            public bool Value { get; set; }
+            public static implicit operator bool(BoolWrapper boolWrapper)
+            {
+                return boolWrapper.Value;
+            }
+            public static implicit operator BoolWrapper(bool boolWrapper)
+            {
+                return new BoolWrapper { Value = boolWrapper };
+            }
         }
     }
 }
