f16acd64b222e6ce96af9d364ee9770d87692756
DrunkRussianGun <maxim71515@gmail.com>
2019-04-19 16:03:39 +0300

    Implement auto reindexing

---
+++ Search.IndexService/Reindexer.cs
@@ -0,0 +1,51 @@
+﻿using Search.Infrastructure;
+using System;
+using System.Threading;
+
+namespace Search.IndexService
+{
+    public class Reindexer
+    {
+        public Reindexer(
+            ElasticSearchClient client,
+            Indexer indexer,
+            bool autoReindexing = false,
+            TimeSpan? indexingFrequency = null,
+            TimeSpan? indexingDelay = null)
+        {
+            _client = client;
+            _indexer = indexer;
+            
+            if (autoReindexing)
+                _indexingTimer = new Timer(
+                    ReindexAll,
+                    null,
+                    indexingDelay ?? default(TimeSpan),
+                    indexingFrequency ?? TimeSpan.FromMinutes(15));
+        }
+
+        private readonly ElasticSearchClient _client;
+        private readonly Indexer _indexer;
+        private readonly Timer _indexingTimer;
+
+        public void ReindexAll() => ReindexAll(null);
+
+        private void ReindexAll(object state)
+        {
+            throw new NotImplementedException();
+
+            var response = _client.Search(desc => desc
+                .Query(query => query.MatchAll())
+            );
+
+            foreach (var document in response.Documents)
+            {
+                var indexRequest = new IndexRequest
+                {
+                    //Url = document.Url
+                };
+                _indexer.Index(indexRequest);
+            }
+        }
+    }
+}
