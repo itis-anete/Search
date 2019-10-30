93b223ddcabfda5cae15e468252662d75e8f887d
Daryayan <daryasa1991@mail.ru>
2019-10-30 20:25:08 +0300

    Добавлен список вложенных в html ссылок в класс ParsedHtml.

--- Search.IndexService/Internal/ParsedHtml.cs
+++ Search.IndexService/Internal/ParsedHtml.cs
@@ -1,8 +1,14 @@
-﻿namespace Search.IndexService.Internal
+﻿using System.Collections.Generic;
+
+namespace Search.IndexService.Internal
 {
     internal class ParsedHtml
     {
         public string Title { get; set; }
+
         public string Text { get; set; }
+
+        public List<string> Links;
+
     }
 }
