e415f80f5e6b1fa7677a713cf44b2bfbd38ec6ab
Drunk Russian Gun <drunkrussiangun@gmail.com>
2019-11-21 22:36:25 +0300

    Добавил самописный Result для обработки ошибок

---
+++ RailwayResults/Result.cs
@@ -0,0 +1,57 @@
+﻿namespace RailwayResults
+{
+    public class Result<TError>
+    {
+        public TError Error { get; }
+        
+        public bool IsSuccess { get; }
+
+        public bool IsFailure => !IsSuccess;
+
+        protected Result(bool isSuccess, TError error)
+        {
+            IsSuccess = isSuccess;
+            Error = error;
+        }
+
+        public static Result<TError> Fail(TError error)
+        {
+            return new Result<TError>(false, error);
+        }
+
+        public static Result<TError> Success()
+        {
+            return new Result<TError>(true, default(TError));
+        }
+
+        public static Result<TError> Combine(params Result<TError>[] results)
+        {
+            foreach (var result in results)
+                if (result.IsFailure)
+                    return result;
+
+            return Success();
+        }
+    }
+
+    public class Result<TValue, TError> : Result<TError>
+    {
+        public TValue Value { get; }
+
+        protected Result(bool success, TValue value, TError error)
+            : base(success, error)
+        {
+            Value = value;
+        }
+
+        public static Result<TValue, TError> Success(TValue value)
+        {
+            return new Result<TValue, TError>(true, value, default(TError));
+        }
+
+        public new static Result<TValue, TError> Fail(TError error)
+        {
+            return new Result<TValue, TError>(false, default(TValue), error);
+        }
+    }
+}
---
+++ RailwayResults/ResultExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace RailwayResults
+{
+    public static class ResultExtensions
+    {
+        public static Result<TValue, TError> Success<TValue, TError>(this TValue value)
+        {
+            return Result<TValue, TError>.Success(value);
+        }
+    }
+}
