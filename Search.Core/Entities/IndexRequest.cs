0a39173e21df33fdf588b20921bf49a28900bef2
Drunk Russian Gun <drunkrussiangun@gmail.com>
2019-11-21 15:39:44 +0300

    Определил модель IndexRequest

---
+++ Search.Core/Entities/IndexRequest.cs
@@ -0,0 +1,20 @@
+﻿using Search.Core.Entities;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Search.IndexService
+{
+    public class IndexRequest
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        public Uri Url { get; set; }
+
+        public DateTime CreatedTime { get; set; }
+
+        public DateTime IndexedTime { get; set; }
+
+        public IndexRequestStatus Status { get; set; }
+    }
+}
---
+++ Search.Core/Entities/IndexRequestStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace Search.Core.Entities
+{
+    public enum IndexRequestStatus
+    {
+        Pending = 1,
+        InProgress = 2,
+        Indexed = 3
+    }
+}
