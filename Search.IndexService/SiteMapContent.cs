c40b06f8e0a8629feea1b35dbb6d6245b7550d54
Daryayan <daryasa1991@mail.ru>
2019-11-30 20:16:33 +0300

    * Добавлен класс контента SiteMap * Добавлен класс получения контента SiteMap

---
+++ Search.IndexService/SiteMapContent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/// <summary>
+/// Объект - контента Sitemap сайта
+/// </summary>
+namespace Search.IndexService
+{
+    internal class SiteMapContent
+    {
+        public Uri Url { get; set; }
+
+        public List<string> Links { get; set; }
+
+        public List<string> Priority { get; set; }
+
+        public List<string> LastModified { get; set; }
+
+        public List<string> ChangeFrequency { get; set; }
+
+    }
+}
