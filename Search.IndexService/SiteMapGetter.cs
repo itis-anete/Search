135fea5ee54a9b2c4fbecdb0a8e725a60d052e2b
Daryayan <daryasa1991@mail.ru>
2019-11-30 20:16:33 +0300

    * Добавлен класс контента SiteMap * Добавлен класс получения контента SiteMap

---
+++ Search.IndexService/SiteMapGetter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace Search.IndexService
+{
+    /// <summary>
+    /// Класс получения контента SiteMap
+    /// </summary>
+    internal class SiteMapGetter
+    {
+        public static SiteMapContent GetContetn(Uri url)
+        {
+            var doc = new XmlDocument();
+            doc.LoadXml($"{url}/sitemap.xml");
+
+            var links = new List<string>();
+            var priority = new List<string>();
+            var lastModified = new List<string>();
+            var changeFreq = new List<string>();
+
+            var xnList = doc.GetElementsByTagName("url");
+            foreach (XmlNode node in xnList)
+            {
+                links.Add(node["loc"].InnerText);
+                priority.Add(node["priority"].InnerText);
+                lastModified.Add(node["lastmod"].InnerText);
+                changeFreq.Add(node["changefreq"].InnerText);
+            }
+
+            return new SiteMapContent()
+            {
+                Url = url,
+                Links = links,
+                Priority = priority,
+                LastModified = lastModified,
+                ChangeFrequency = changeFreq
+            };
+        }
+
+    }
+}
