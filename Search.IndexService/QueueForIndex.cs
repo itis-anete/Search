9e9ad426d9d8ac8e17c3ae434b79ca4a950f09b8
stwer1998 <stwer1998@mail.ru>
2019-12-01 14:35:59 +0300

    Исправил работу с Elastic

--- Search.IndexService/QueueForIndex.cs
+++ Search.IndexService/QueueForIndex.cs
@@ -2,37 +2,55 @@
 using Search.Core.Entities;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Search.IndexService
 {
     public class QueueForIndex
     {
-        public QueueForIndex(ElasticSearchClient<Document> client, ElasticSearchOptions options)
+        public QueueForIndex(ElasticSearchClient<IndexRequest> client, ElasticSearchOptions options)
         {
             _client = client;
             _options = options;
         }
 
-        private readonly ElasticSearchClient<Document> _client;
+        private readonly ElasticSearchClient<IndexRequest> _client;
         private readonly ElasticSearchOptions _options;
 
         public void AddToQueueElement(IndexRequest request)
         {
             //methot for adding element to elastic
-            var el = new IndexRequest()
+            var indexLink = new IndexRequest()
             {
                 CreatedTime = DateTime.Now,
-                Url = request.Url
+                Url = request.Url,
+                Status = IndexRequestStatus.Pending                
             };
 
-            //_client.AddQueueIndex(el, x => x.Id(el.Uri.ToString()).Index(_options.DocumentsIndexName));
+            _client.Index(indexLink, x => x.Id(indexLink.Url.ToString()).Index(_options.DocumentsIndexName));
         }
 
         public IndexRequest GetIndexElement()
-        {
-            return null;//method for return next link for index
+        {//method for return next link for index
+            var responseFromElastic = _client.Search(search => search.Index(_options.DocumentsIndexName).
+              Query(desc => desc.Match(m => m.Field(x => x.Status==IndexRequestStatus.Pending))));
+
+
+            var result = responseFromElastic.Documents
+                .Select(x => new IndexRequest
+                {
+                    Url = x.Url,
+                    CreatedTime = x.CreatedTime,
+                    Status = x.Status
+                }).OrderBy(s=>s.CreatedTime).First();
+            if (result == null)
+            {
+                return null;
+            }
+            else  return result;
         }
+
 
     }
 }
