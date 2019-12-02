c1a109a4385c9b2255b8bcd1ee98233ff33b7742
stwer1998 <stwer1998@mail.ru>
2019-12-02 20:18:12 +0300

    Change Reindexer and delete QueueForReindex

--- Search.IndexService/Reindexer.cs
+++ Search.IndexService/Reindexer.cs
@@ -1,6 +1,7 @@
 ﻿using Search.Core.Elasticsearch;
 using Search.Core.Entities;
 using System;
+using System.Linq;
 using System.Threading;
 
 namespace Search.IndexService
@@ -11,41 +12,66 @@ namespace Search.IndexService
             ElasticSearchClient<Document> client,
             ElasticSearchOptions options,
             Indexer indexer,
-            bool autoReindexing = false,
-            TimeSpan? indexingFrequency = null,
-            TimeSpan? firstIndexingDeferral = null)
+            //bool autoReindexing = false,
+            //TimeSpan? indexingFrequency = null,
+            QueueForIndex queueForIndex)
+        // TimeSpan? firstIndexingDeferral = null)
         {
             _client = client;
             _options = options;
             _indexer = indexer;
-            
-            if (autoReindexing)
-                _indexingTimer = new Timer(
-                    ReindexAll,
-                    null,
-                    firstIndexingDeferral ?? default(TimeSpan),
-                    indexingFrequency ?? TimeSpan.FromMinutes(15));
+            ReindexTime = -7;
+            _queueForIndex = queueForIndex;
+            //if (autoReindexing)
+            //    _indexingTimer = new Timer(
+            //        ReindexAll,
+            //        null,
+            //        firstIndexingDeferral ?? default(TimeSpan),
+            //        indexingFrequency ?? TimeSpan.FromMinutes(15));
         }
 
-        public void ReindexAll() => ReindexAll(null);
-
+        //public void ReindexAll() => ReindexAll(null);
+        private int ReindexTime { get; set; }
         private readonly ElasticSearchClient<Document> _client;
         private readonly ElasticSearchOptions _options;
         private readonly Indexer _indexer;
         private readonly Timer _indexingTimer;
+        private QueueForIndex _queueForIndex;
 
-        private void ReindexAll(object state)
+        //private void ReindexAll(object state)
+        //{
+        //    var response = _client.Search(desc => desc
+        //        .Index(_options.DocumentsIndexName)
+        //        .Query(query => query.MatchAll())
+        //    );
+
+        //    foreach (var document in response.Documents)
+        //    {
+        //        var indexRequest = new IndexRequest { Url = document.Url };
+        //        _indexer.Index(indexRequest);
+        //    }
+        //}
+
+        public void SearchOldPages()
         {
-            var response = _client.Search(desc => desc
-                .Index(_options.DocumentsIndexName)
-                .Query(query => query.MatchAll())
-            );
+            var responseFromElastic = _client.Search(search => search
+               .Index(_options.DocumentsIndexName)
+               .Query(desc => desc
+                   .Match(match => match
+                       .Field(x => x.IndexedTime < DateTime.Now.AddDays(ReindexTime)))));
+            var oldPages = responseFromElastic.Documents
+                .Select(doc => new IndexRequest
+                {
+                    Url = doc.Url,
+                    CreatedTime = DateTime.Now,
+                    Status = IndexRequestStatus.Pending
+                }).ToList();
 
-            foreach (var document in response.Documents)
+            foreach (var item in oldPages)
             {
-                var indexRequest = new IndexRequest { Url = document.Url };
-                _indexer.Index(indexRequest);
+                _queueForIndex.AddToQueueElement(item);
             }
+
         }
     }
 }
