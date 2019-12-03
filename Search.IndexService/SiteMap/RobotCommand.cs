978e0f148073be197460f6e46f5e71e8e526cdcb
Daryayan <daryasa1991@mail.ru>
2019-12-03 17:56:14 +0300

    #NERABOCHIE-20 * Добавлены класс Robot и RobotCommand для возможности работы c sitemap.xml

---
+++ Search.IndexService/SiteMap/RobotCommand.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Search.IndexService.SiteMap
+{
+    class RobotCommand
+    {
+        private string command;
+
+        private string url = string.Empty;
+
+        private string useragent = string.Empty;
+
+        public RobotCommand(string commandline)
+        {
+            int PosOfComment = commandline.IndexOf('#');
+            if (PosOfComment == 0)
+            {
+                command = "COMMENT";
+            }
+            else
+            {
+                if (PosOfComment >= 0)
+                {
+                    commandline = commandline.Substring(0, PosOfComment);
+                }
+                if (commandline.Length > 0)
+                {
+                    string[] lineArray = commandline.Split(':');
+                    command = lineArray[0].Trim().ToLower();
+                    if (lineArray.Length > 1)
+                    {
+                        if (command == "user-agent")
+                        {
+                            useragent = lineArray[1].Trim();
+                        }
+                        else
+                        {
+                            url = lineArray[1].Trim();
+                            if (lineArray.Length > 2)
+                            {
+                                url += ":" + lineArray[2].Trim();
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+
+        public string Command
+        {
+            get { return command; }
+        }
+
+        public string Url
+        {
+            get { return url; }
+        }
+
+        public string UserAgent
+        {
+            get { return useragent; }
+        }
+    }
+}
