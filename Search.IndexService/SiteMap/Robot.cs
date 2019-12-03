077cd0881986e7867e3990d7381f4ddc95e4e5a6
Daryayan <daryasa1991@mail.ru>
2019-12-03 18:45:24 +0300

    #NERABOCHIE-20 * Получение контента страницы по Url с помощью User-Agent * Добавлен обработчик ошибок для получения https статуса

--- Search.IndexService/SiteMap/Robot.cs
+++ Search.IndexService/SiteMap/Robot.cs
@@ -25,6 +25,90 @@ namespace Search.IndexService.SiteMap
 
         private static ArrayList BlockedUrls = new ArrayList();
 
+        static string getURLContent(string URL)
+        {
+            Console.WriteLine("Access URL: " + URL);
+            Console.WriteLine("Proxy: " + proxyServer);
+            Console.WriteLine("User Agent: " + robotAgent);
+
+            content = string.Empty;
+            lastError = ""; 
+            statusCode = -1;
+
+            WebClient client = new WebClient();
+
+            if (!String.IsNullOrEmpty(proxyServer))
+            {
+                if (proxy == null)
+                {                 
+                    proxy = new WebProxy(proxyServer, true);
+                    client.Proxy = proxy;
+                }
+            }
+            
+            client.Headers["User-Agent"] = robotAgent; 
+
+            client.Encoding = System.Text.Encoding.UTF8;
+
+            try
+            {
+                content = client.DownloadString(URL);
+                statusCode = 200; 
+            }
+            catch (WebException ex)
+            {
+                if (ex.Status == WebExceptionStatus.NameResolutionFailure)
+                {
+                    lastError = "Bad Domain Name";
+                }
+                else if (ex.Status == WebExceptionStatus.ProtocolError)
+                {
+                    HttpWebResponse HTTPResponse = (HttpWebResponse)ex.Response;
+
+                    statusCode = (int)HTTPResponse.StatusCode;
+
+                    if (HTTPResponse.StatusCode == HttpStatusCode.NotFound)
+                    {
+                        lastError = "Page Not Found";
+                    }
+                    else if (HTTPResponse.StatusCode == HttpStatusCode.Forbidden)
+                    {
+                        lastError = "Access Forbidden";
+                    }
+                    else if (HTTPResponse.StatusCode == HttpStatusCode.InternalServerError)
+                    {
+                        lastError = "Server Error";
+                    }
+                    else if (HTTPResponse.StatusCode == HttpStatusCode.Gone)
+                    {
+                        lastError = "Page No Longer Available";
+                    }
+                    else
+                    {
+                        lastError = HTTPResponse.StatusDescription;
+                    }
+                }
+                else
+                {
+                    lastError = "Error: " + ex.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                lastError = "Error: " + ex.ToString();
+            }
+            finally
+            {
+                client.Dispose();
+            }
+
+            if (!String.IsNullOrEmpty(lastError))
+            {
+                Console.WriteLine(statusCode.ToString() + ": " + lastError);
+            }
+
+            return content;
+        }
 
     }
 }
