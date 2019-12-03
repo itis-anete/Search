87d6ff96dbdfb2432342f2d5ab7027a4ab7509ea
Daryayan <daryasa1991@mail.ru>
2019-12-03 17:56:14 +0300

    #NERABOCHIE-20 * Добавлены класс Robot и RobotCommand для возможности работы c sitemap.xml

---
+++ Search.IndexService/SiteMap/Robot.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Net;
+
+namespace Search.IndexService.SiteMap
+{
+    /// <summary>
+    /// Класс-парсер для robot.txt 
+    /// для скачивания sitemap.xml сайта
+    /// </summary>
+    class Robot
+    {
+        private static string robotAgent = "Mozilla 5.0; RobsRobot 1.2; www.strictly-software.com;";
+
+        private static string proxyServer = ""; 
+
+        private static WebProxy proxy;
+
+        private static string content = string.Empty;
+
+        private static int statusCode = -1;
+
+        private static string lastError = string.Empty;
+
+        private static ArrayList BlockedUrls = new ArrayList();
+
+
+    }
+}
