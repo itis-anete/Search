f5373383fee812d65df9b58a39385c607a881cf5
Drunk Russian Gun <drunkrussiangun@gmail.com>
2019-12-19 03:40:14 +0300

    Добавил проект IndexHelpers на F#, в котором реализовано ограничение количества страниц на сайт

---
+++ Search.IndexHelpers/PagesPerSiteLimiter.fs
@@ -0,0 +1,17 @@
+﻿namespace Search.IndexHelpers
+
+open Microsoft.Extensions.Options;
+
+type PagesPerSiteLimiter private() =
+    [<DefaultValue>]
+    val mutable private pagesPerSiteLimit: int
+    member public this.PagesPerSiteLimit
+        with get() = this.pagesPerSiteLimit
+
+    public new(options: IOptionsMonitor<PagesPerSiteLimiterOptions>) as this =
+        PagesPerSiteLimiter()
+        then
+            this.pagesPerSiteLimit <- options.CurrentValue.PagesPerSiteLimit
+
+    member this.IsLimitReached(pagesCount: int) =
+        pagesCount > this.pagesPerSiteLimit
---
+++ Search.IndexHelpers/PagesPerSiteLimiterOptions.fs
@@ -0,0 +1,8 @@
+﻿namespace Search.IndexHelpers
+
+type PagesPerSiteLimiterOptions() =
+    [<DefaultValue>]
+    val mutable private pagesPerSiteLimit: int
+    member this.PagesPerSiteLimit
+        with get() = this.pagesPerSiteLimit
+        and set(value) = this.pagesPerSiteLimit <- value
